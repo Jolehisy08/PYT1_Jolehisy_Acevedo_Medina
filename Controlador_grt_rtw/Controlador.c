4d380f9cb1de71f87d98ee687276655ea782413f
Jolehisy08 <jamedina3802@gmail.com>
2026-03-17 19:51:55 +0000

    Código C++ del controlador

---
+++ Controlador_grt_rtw/Controlador.c
@@ -0,0 +1,250 @@
+/*
+ * Controlador.c
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "Controlador".
+ *
+ * Model version              : 1.13
+ * Simulink Coder version : 24.2 (R2024b) 21-Jun-2024
+ * C source code generated on : Tue Mar 17 19:49:27 2026
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "Controlador.h"
+#include "rtwtypes.h"
+#include <string.h>
+#include "Controlador_private.h"
+
+/* Block states (default storage) */
+DW_Controlador_T Controlador_DW;
+
+/* External inputs (root inport signals with default storage) */
+ExtU_Controlador_T Controlador_U;
+
+/* External outputs (root outports fed by signals with default storage) */
+ExtY_Controlador_T Controlador_Y;
+
+/* Real-time model */
+static RT_MODEL_Controlador_T Controlador_M_;
+RT_MODEL_Controlador_T *const Controlador_M = &Controlador_M_;
+
+/* Model step function */
+void Controlador_step(void)
+{
+  real_T rtb_DeadZone;
+  real_T rtb_FilterCoefficient;
+  real_T rtb_IntegralGain;
+  real_T rtb_Sum;
+  int8_T tmp;
+  int8_T tmp_0;
+
+  /* Outport: '<Root>/Señal de control' incorporates:
+   *  DiscreteTransferFcn: '<S1>/Discrete Transfer Fcn'
+   */
+  Controlador_Y.Sealdecontrol = Controlador_P.DiscreteTransferFcn_NumCoef *
+    Controlador_DW.DiscreteTransferFcn_states;
+
+  /* Sum: '<S1>/Sum' incorporates:
+   *  Inport: '<Root>/Thrust set point (N)'
+   *  Inport: '<Root>/Thust medido (N)'
+   */
+  rtb_IntegralGain = Controlador_U.ThrustsetpointN - Controlador_U.ThustmedidoN;
+
+  /* Gain: '<S42>/Filter Coefficient' incorporates:
+   *  DiscreteIntegrator: '<S34>/Filter'
+   *  Gain: '<S32>/Derivative Gain'
+   *  Sum: '<S34>/SumD'
+   */
+  rtb_FilterCoefficient = (Controlador_P.Controlador_kd_mask * rtb_IntegralGain
+    - Controlador_DW.Filter_DSTATE) * Controlador_P.DiscretePIDController_N;
+
+  /* Sum: '<S48>/Sum' incorporates:
+   *  DiscreteIntegrator: '<S39>/Integrator'
+   *  Gain: '<S44>/Proportional Gain'
+   */
+  rtb_Sum = (Controlador_P.Controlador_kp_mask * rtb_IntegralGain +
+             Controlador_DW.Integrator_DSTATE) + rtb_FilterCoefficient;
+
+  /* DeadZone: '<S31>/DeadZone' incorporates:
+   *  Saturate: '<S46>/Saturation'
+   */
+  if (rtb_Sum > Controlador_P.DiscretePIDController_UpperSatu) {
+    rtb_DeadZone = rtb_Sum - Controlador_P.DiscretePIDController_UpperSatu;
+    rtb_Sum = Controlador_P.DiscretePIDController_UpperSatu;
+  } else {
+    if (rtb_Sum >= Controlador_P.DiscretePIDController_LowerSatu) {
+      rtb_DeadZone = 0.0;
+    } else {
+      rtb_DeadZone = rtb_Sum - Controlador_P.DiscretePIDController_LowerSatu;
+    }
+
+    if (rtb_Sum < Controlador_P.DiscretePIDController_LowerSatu) {
+      rtb_Sum = Controlador_P.DiscretePIDController_LowerSatu;
+    }
+  }
+
+  /* End of DeadZone: '<S31>/DeadZone' */
+
+  /* Gain: '<S36>/Integral Gain' */
+  rtb_IntegralGain *= Controlador_P.Controlador_ki_mask;
+
+  /* Update for DiscreteTransferFcn: '<S1>/Discrete Transfer Fcn' incorporates:
+   *  Saturate: '<S46>/Saturation'
+   */
+  Controlador_DW.DiscreteTransferFcn_states = (rtb_Sum -
+    Controlador_P.DiscreteTransferFcn_DenCoef[1] *
+    Controlador_DW.DiscreteTransferFcn_states) /
+    Controlador_P.DiscreteTransferFcn_DenCoef[0];
+
+  /* Switch: '<S29>/Switch1' incorporates:
+   *  Constant: '<S29>/Clamping_zero'
+   *  Constant: '<S29>/Constant'
+   *  Constant: '<S29>/Constant2'
+   *  RelationalOperator: '<S29>/fix for DT propagation issue'
+   */
+  if (rtb_DeadZone > Controlador_P.Clamping_zero_Value) {
+    tmp = Controlador_P.Constant_Value;
+  } else {
+    tmp = Controlador_P.Constant2_Value;
+  }
+
+  /* Switch: '<S29>/Switch2' incorporates:
+   *  Constant: '<S29>/Clamping_zero'
+   *  Constant: '<S29>/Constant3'
+   *  Constant: '<S29>/Constant4'
+   *  RelationalOperator: '<S29>/fix for DT propagation issue1'
+   */
+  if (rtb_IntegralGain > Controlador_P.Clamping_zero_Value) {
+    tmp_0 = Controlador_P.Constant3_Value;
+  } else {
+    tmp_0 = Controlador_P.Constant4_Value;
+  }
+
+  /* Switch: '<S29>/Switch' incorporates:
+   *  Constant: '<S29>/Clamping_zero'
+   *  Constant: '<S29>/Constant1'
+   *  Logic: '<S29>/AND3'
+   *  RelationalOperator: '<S29>/Equal1'
+   *  RelationalOperator: '<S29>/Relational Operator'
+   *  Switch: '<S29>/Switch1'
+   *  Switch: '<S29>/Switch2'
+   */
+  if ((Controlador_P.Clamping_zero_Value != rtb_DeadZone) && (tmp == tmp_0)) {
+    rtb_IntegralGain = Controlador_P.Constant1_Value;
+  }
+
+  /* Update for DiscreteIntegrator: '<S39>/Integrator' incorporates:
+   *  Switch: '<S29>/Switch'
+   */
+  Controlador_DW.Integrator_DSTATE += Controlador_P.Integrator_gainval *
+    rtb_IntegralGain;
+
+  /* Update for DiscreteIntegrator: '<S34>/Filter' */
+  Controlador_DW.Filter_DSTATE += Controlador_P.Filter_gainval *
+    rtb_FilterCoefficient;
+
+  /* Matfile logging */
+  rt_UpdateTXYLogVars(Controlador_M->rtwLogInfo,
+                      (&Controlador_M->Timing.taskTime0));
+
+  /* signal main to stop simulation */
+  {                                    /* Sample time: [0.01s, 0.0s] */
+    if ((rtmGetTFinal(Controlador_M)!=-1) &&
+        !((rtmGetTFinal(Controlador_M)-Controlador_M->Timing.taskTime0) >
+          Controlador_M->Timing.taskTime0 * (DBL_EPSILON))) {
+      rtmSetErrorStatus(Controlador_M, "Simulation finished");
+    }
+  }
+
+  /* Update absolute time for base rate */
+  /* The "clockTick0" counts the number of times the code of this task has
+   * been executed. The absolute time is the multiplication of "clockTick0"
+   * and "Timing.stepSize0". Size of "clockTick0" ensures timer will not
+   * overflow during the application lifespan selected.
+   * Timer of this task consists of two 32 bit unsigned integers.
+   * The two integers represent the low bits Timing.clockTick0 and the high bits
+   * Timing.clockTickH0. When the low bit overflows to 0, the high bits increment.
+   */
+  if (!(++Controlador_M->Timing.clockTick0)) {
+    ++Controlador_M->Timing.clockTickH0;
+  }
+
+  Controlador_M->Timing.taskTime0 = Controlador_M->Timing.clockTick0 *
+    Controlador_M->Timing.stepSize0 + Controlador_M->Timing.clockTickH0 *
+    Controlador_M->Timing.stepSize0 * 4294967296.0;
+}
+
+/* Model initialize function */
+void Controlador_initialize(void)
+{
+  /* Registration code */
+
+  /* initialize real-time model */
+  (void) memset((void *)Controlador_M, 0,
+                sizeof(RT_MODEL_Controlador_T));
+  rtmSetTFinal(Controlador_M, 10.0);
+  Controlador_M->Timing.stepSize0 = 0.01;
+
+  /* Setup for data logging */
+  {
+    static RTWLogInfo rt_DataLoggingInfo;
+    rt_DataLoggingInfo.loggingInterval = (NULL);
+    Controlador_M->rtwLogInfo = &rt_DataLoggingInfo;
+  }
+
+  /* Setup for data logging */
+  {
+    rtliSetLogXSignalInfo(Controlador_M->rtwLogInfo, (NULL));
+    rtliSetLogXSignalPtrs(Controlador_M->rtwLogInfo, (NULL));
+    rtliSetLogT(Controlador_M->rtwLogInfo, "tout");
+    rtliSetLogX(Controlador_M->rtwLogInfo, "");
+    rtliSetLogXFinal(Controlador_M->rtwLogInfo, "");
+    rtliSetLogVarNameModifier(Controlador_M->rtwLogInfo, "rt_");
+    rtliSetLogFormat(Controlador_M->rtwLogInfo, 4);
+    rtliSetLogMaxRows(Controlador_M->rtwLogInfo, 0);
+    rtliSetLogDecimation(Controlador_M->rtwLogInfo, 1);
+    rtliSetLogY(Controlador_M->rtwLogInfo, "");
+    rtliSetLogYSignalInfo(Controlador_M->rtwLogInfo, (NULL));
+    rtliSetLogYSignalPtrs(Controlador_M->rtwLogInfo, (NULL));
+  }
+
+  /* states (dwork) */
+  (void) memset((void *)&Controlador_DW, 0,
+                sizeof(DW_Controlador_T));
+
+  /* external inputs */
+  (void)memset(&Controlador_U, 0, sizeof(ExtU_Controlador_T));
+
+  /* external outputs */
+  Controlador_Y.Sealdecontrol = 0.0;
+
+  /* Matfile logging */
+  rt_StartDataLoggingWithStartTime(Controlador_M->rtwLogInfo, 0.0, rtmGetTFinal
+    (Controlador_M), Controlador_M->Timing.stepSize0, (&rtmGetErrorStatus
+    (Controlador_M)));
+
+  /* InitializeConditions for DiscreteTransferFcn: '<S1>/Discrete Transfer Fcn' */
+  Controlador_DW.DiscreteTransferFcn_states =
+    Controlador_P.DiscreteTransferFcn_InitialStat;
+
+  /* InitializeConditions for DiscreteIntegrator: '<S39>/Integrator' */
+  Controlador_DW.Integrator_DSTATE =
+    Controlador_P.DiscretePIDController_Initial_j;
+
+  /* InitializeConditions for DiscreteIntegrator: '<S34>/Filter' */
+  Controlador_DW.Filter_DSTATE = Controlador_P.DiscretePIDController_InitialCo;
+}
+
+/* Model terminate function */
+void Controlador_terminate(void)
+{
+  /* (no terminate code required) */
+}
---
+++ Controlador_grt_rtw/Controlador.h
@@ -0,0 +1,281 @@
+/*
+ * Controlador.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "Controlador".
+ *
+ * Model version              : 1.13
+ * Simulink Coder version : 24.2 (R2024b) 21-Jun-2024
+ * C source code generated on : Tue Mar 17 19:49:27 2026
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef Controlador_h_
+#define Controlador_h_
+#ifndef Controlador_COMMON_INCLUDES_
+#define Controlador_COMMON_INCLUDES_
+#include "rtwtypes.h"
+#include "rtw_continuous.h"
+#include "rtw_solver.h"
+#include "rt_logging.h"
+#include "rt_nonfinite.h"
+#include "math.h"
+#endif                                 /* Controlador_COMMON_INCLUDES_ */
+
+#include "Controlador_types.h"
+#include <float.h>
+#include <string.h>
+#include <stddef.h>
+
+/* Macros for accessing real-time model data structure */
+#ifndef rtmGetFinalTime
+#define rtmGetFinalTime(rtm)           ((rtm)->Timing.tFinal)
+#endif
+
+#ifndef rtmGetRTWLogInfo
+#define rtmGetRTWLogInfo(rtm)          ((rtm)->rtwLogInfo)
+#endif
+
+#ifndef rtmGetErrorStatus
+#define rtmGetErrorStatus(rtm)         ((rtm)->errorStatus)
+#endif
+
+#ifndef rtmSetErrorStatus
+#define rtmSetErrorStatus(rtm, val)    ((rtm)->errorStatus = (val))
+#endif
+
+#ifndef rtmGetStopRequested
+#define rtmGetStopRequested(rtm)       ((rtm)->Timing.stopRequestedFlag)
+#endif
+
+#ifndef rtmSetStopRequested
+#define rtmSetStopRequested(rtm, val)  ((rtm)->Timing.stopRequestedFlag = (val))
+#endif
+
+#ifndef rtmGetStopRequestedPtr
+#define rtmGetStopRequestedPtr(rtm)    (&((rtm)->Timing.stopRequestedFlag))
+#endif
+
+#ifndef rtmGetT
+#define rtmGetT(rtm)                   ((rtm)->Timing.taskTime0)
+#endif
+
+#ifndef rtmGetTFinal
+#define rtmGetTFinal(rtm)              ((rtm)->Timing.tFinal)
+#endif
+
+#ifndef rtmGetTPtr
+#define rtmGetTPtr(rtm)                (&(rtm)->Timing.taskTime0)
+#endif
+
+/* Block states (default storage) for system '<Root>' */
+typedef struct {
+  real_T DiscreteTransferFcn_states;   /* '<S1>/Discrete Transfer Fcn' */
+  real_T Integrator_DSTATE;            /* '<S39>/Integrator' */
+  real_T Filter_DSTATE;                /* '<S34>/Filter' */
+} DW_Controlador_T;
+
+/* External inputs (root inport signals with default storage) */
+typedef struct {
+  real_T ThrustsetpointN;              /* '<Root>/Thrust set point (N)' */
+  real_T ThustmedidoN;                 /* '<Root>/Thust medido (N)' */
+} ExtU_Controlador_T;
+
+/* External outputs (root outports fed by signals with default storage) */
+typedef struct {
+  real_T Sealdecontrol;                /* '<Root>/Señal de control' */
+} ExtY_Controlador_T;
+
+/* Parameters (default storage) */
+struct P_Controlador_T_ {
+  real_T DiscretePIDController_InitialCo;
+                              /* Mask Parameter: DiscretePIDController_InitialCo
+                               * Referenced by: '<S34>/Filter'
+                               */
+  real_T DiscretePIDController_Initial_j;
+                              /* Mask Parameter: DiscretePIDController_Initial_j
+                               * Referenced by: '<S39>/Integrator'
+                               */
+  real_T DiscretePIDController_LowerSatu;
+                              /* Mask Parameter: DiscretePIDController_LowerSatu
+                               * Referenced by:
+                               *   '<S46>/Saturation'
+                               *   '<S31>/DeadZone'
+                               */
+  real_T DiscretePIDController_N;     /* Mask Parameter: DiscretePIDController_N
+                                       * Referenced by: '<S42>/Filter Coefficient'
+                                       */
+  real_T DiscretePIDController_UpperSatu;
+                              /* Mask Parameter: DiscretePIDController_UpperSatu
+                               * Referenced by:
+                               *   '<S46>/Saturation'
+                               *   '<S31>/DeadZone'
+                               */
+  real_T Controlador_kd_mask;          /* Mask Parameter: Controlador_kd_mask
+                                        * Referenced by: '<S32>/Derivative Gain'
+                                        */
+  real_T Controlador_ki_mask;          /* Mask Parameter: Controlador_ki_mask
+                                        * Referenced by: '<S36>/Integral Gain'
+                                        */
+  real_T Controlador_kp_mask;          /* Mask Parameter: Controlador_kp_mask
+                                        * Referenced by: '<S44>/Proportional Gain'
+                                        */
+  real_T Constant1_Value;              /* Expression: 0
+                                        * Referenced by: '<S29>/Constant1'
+                                        */
+  real_T DiscreteTransferFcn_NumCoef;  /* Expression: [num]
+                                        * Referenced by: '<S1>/Discrete Transfer Fcn'
+                                        */
+  real_T DiscreteTransferFcn_DenCoef[2];/* Expression: [den1 den2]
+                                         * Referenced by: '<S1>/Discrete Transfer Fcn'
+                                         */
+  real_T DiscreteTransferFcn_InitialStat;/* Expression: 0
+                                          * Referenced by: '<S1>/Discrete Transfer Fcn'
+                                          */
+  real_T Clamping_zero_Value;          /* Expression: 0
+                                        * Referenced by: '<S29>/Clamping_zero'
+                                        */
+  real_T Integrator_gainval;           /* Computed Parameter: Integrator_gainval
+                                        * Referenced by: '<S39>/Integrator'
+                                        */
+  real_T Filter_gainval;               /* Computed Parameter: Filter_gainval
+                                        * Referenced by: '<S34>/Filter'
+                                        */
+  int8_T Constant_Value;               /* Computed Parameter: Constant_Value
+                                        * Referenced by: '<S29>/Constant'
+                                        */
+  int8_T Constant2_Value;              /* Computed Parameter: Constant2_Value
+                                        * Referenced by: '<S29>/Constant2'
+                                        */
+  int8_T Constant3_Value;              /* Computed Parameter: Constant3_Value
+                                        * Referenced by: '<S29>/Constant3'
+                                        */
+  int8_T Constant4_Value;              /* Computed Parameter: Constant4_Value
+                                        * Referenced by: '<S29>/Constant4'
+                                        */
+};
+
+/* Real-time Model Data Structure */
+struct tag_RTM_Controlador_T {
+  const char_T *errorStatus;
+  RTWLogInfo *rtwLogInfo;
+
+  /*
+   * Timing:
+   * The following substructure contains information regarding
+   * the timing information for the model.
+   */
+  struct {
+    time_T taskTime0;
+    uint32_T clockTick0;
+    uint32_T clockTickH0;
+    time_T stepSize0;
+    time_T tFinal;
+    boolean_T stopRequestedFlag;
+  } Timing;
+};
+
+/* Block parameters (default storage) */
+extern P_Controlador_T Controlador_P;
+
+/* Block states (default storage) */
+extern DW_Controlador_T Controlador_DW;
+
+/* External inputs (root inport signals with default storage) */
+extern ExtU_Controlador_T Controlador_U;
+
+/* External outputs (root outports fed by signals with default storage) */
+extern ExtY_Controlador_T Controlador_Y;
+
+/* Model entry point functions */
+extern void Controlador_initialize(void);
+extern void Controlador_step(void);
+extern void Controlador_terminate(void);
+
+/* Real-time Model object */
+extern RT_MODEL_Controlador_T *const Controlador_M;
+
+/*-
+ * The generated code includes comments that allow you to trace directly
+ * back to the appropriate location in the model.  The basic format
+ * is <system>/block_name, where system is the system number (uniquely
+ * assigned by Simulink) and block_name is the name of the block.
+ *
+ * Note that this particular code originates from a subsystem build,
+ * and has its own system numbers different from the parent model.
+ * Refer to the system hierarchy for this subsystem below, and use the
+ * MATLAB hilite_system command to trace the generated code back
+ * to the parent model.  For example,
+ *
+ * hilite_system('Turbine_JAM/Controlador')    - opens subsystem Turbine_JAM/Controlador
+ * hilite_system('Turbine_JAM/Controlador/Kp') - opens and selects block Kp
+ *
+ * Here is the system hierarchy for this model
+ *
+ * '<Root>' : 'Turbine_JAM'
+ * '<S1>'   : 'Turbine_JAM/Controlador'
+ * '<S2>'   : 'Turbine_JAM/Controlador/Discrete PID Controller'
+ * '<S3>'   : 'Turbine_JAM/Controlador/Discrete PID Controller/Anti-windup'
+ * '<S4>'   : 'Turbine_JAM/Controlador/Discrete PID Controller/D Gain'
+ * '<S5>'   : 'Turbine_JAM/Controlador/Discrete PID Controller/External Derivative'
+ * '<S6>'   : 'Turbine_JAM/Controlador/Discrete PID Controller/Filter'
+ * '<S7>'   : 'Turbine_JAM/Controlador/Discrete PID Controller/Filter ICs'
+ * '<S8>'   : 'Turbine_JAM/Controlador/Discrete PID Controller/I Gain'
+ * '<S9>'   : 'Turbine_JAM/Controlador/Discrete PID Controller/Ideal P Gain'
+ * '<S10>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Ideal P Gain Fdbk'
+ * '<S11>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Integrator'
+ * '<S12>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Integrator ICs'
+ * '<S13>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/N Copy'
+ * '<S14>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/N Gain'
+ * '<S15>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/P Copy'
+ * '<S16>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Parallel P Gain'
+ * '<S17>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Reset Signal'
+ * '<S18>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Saturation'
+ * '<S19>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Saturation Fdbk'
+ * '<S20>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Sum'
+ * '<S21>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Sum Fdbk'
+ * '<S22>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Tracking Mode'
+ * '<S23>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Tracking Mode Sum'
+ * '<S24>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Tsamp - Integral'
+ * '<S25>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Tsamp - Ngain'
+ * '<S26>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/postSat Signal'
+ * '<S27>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/preInt Signal'
+ * '<S28>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/preSat Signal'
+ * '<S29>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Anti-windup/Disc. Clamping Parallel'
+ * '<S30>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Anti-windup/Disc. Clamping Parallel/Dead Zone'
+ * '<S31>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Anti-windup/Disc. Clamping Parallel/Dead Zone/Enabled'
+ * '<S32>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/D Gain/Internal Parameters'
+ * '<S33>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/External Derivative/Error'
+ * '<S34>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Filter/Disc. Forward Euler Filter'
+ * '<S35>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Filter ICs/Internal IC - Filter'
+ * '<S36>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/I Gain/Internal Parameters'
+ * '<S37>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Ideal P Gain/Passthrough'
+ * '<S38>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Ideal P Gain Fdbk/Disabled'
+ * '<S39>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Integrator/Discrete'
+ * '<S40>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Integrator ICs/Internal IC'
+ * '<S41>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/N Copy/Disabled'
+ * '<S42>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/N Gain/Internal Parameters'
+ * '<S43>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/P Copy/Disabled'
+ * '<S44>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Parallel P Gain/Internal Parameters'
+ * '<S45>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Reset Signal/Disabled'
+ * '<S46>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Saturation/Enabled'
+ * '<S47>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Saturation Fdbk/Disabled'
+ * '<S48>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Sum/Sum_PID'
+ * '<S49>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Sum Fdbk/Disabled'
+ * '<S50>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Tracking Mode/Disabled'
+ * '<S51>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Tracking Mode Sum/Passthrough'
+ * '<S52>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Tsamp - Integral/TsSignalSpecification'
+ * '<S53>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/Tsamp - Ngain/Passthrough'
+ * '<S54>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/postSat Signal/Forward_Path'
+ * '<S55>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/preInt Signal/Internal PreInt'
+ * '<S56>'  : 'Turbine_JAM/Controlador/Discrete PID Controller/preSat Signal/Forward_Path'
+ */
+#endif                                 /* Controlador_h_ */
